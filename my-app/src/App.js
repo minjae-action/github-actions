f66b2fb1b37577f57fce32bc0b1b83ad3e1750ef
Shaun <86937655+ShaunAUS@users.noreply.github.com>
2024-04-13 19:27:47 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd234
+          Learn GithubAction cicd2345
         </a>
       </header>
     </div>
